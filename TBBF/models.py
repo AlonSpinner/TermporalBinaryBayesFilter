7f10ca88d651e0a97e0d972f9a85dbeef8355a3b
alonspinner <alonspinner@gmail.com>
2022-06-01 18:35:25 +0300

    someth8ing

--- TBBF/models.py
+++ TBBF/models.py
@@ -1,7 +1,8 @@
 from .gaussians import gaussian1D as g1d
 import numpy as np
+EPS = 1e-15
 
-def inverseSensorModel(z : str, S : g1d, x : int, t : float, m = "⬛"):
+def inverseSensorScheduleModel(z : str, S : g1d, x : int, t : float, m = "⬛") -> float:
     '''
     z - meaurement "⬜" or "⬛"
     S - schedule of building
@@ -10,26 +11,40 @@ def inverseSensorModel(z : str, S : g1d, x : int, t : float, m = "⬛"):
     
     returns probablity of m
     '''
-    num = forwardModel(z, m) * scheduleModel(S, x, t , m)
-    denum = forwardModel(z, m = "⬜") * scheduleModel(S, x, t , m = "⬜") + \
-                forwardModel(z, m = "⬛") * scheduleModel(S, x, t , m = "⬛")
+    num = forwardSensorModel(z, m) * scheduleModel(S, x, t , m)
+    denum = forwardSensorModel(z, m = "⬜") * scheduleModel(S, x, t , m = "⬜") + \
+                forwardSensorModel(z, m = "⬛") * scheduleModel(S, x, t , m = "⬛")
     return num/denum
 
-def forwardModel(z : str, m : str):
+def forwardSensorModel(z : str, m : str) -> float:
+    '''
+    z - meaurement "⬜" or "⬛"
+    S - schedule of building
+    x - location of cell
+    t - world time
+    
+    returns probablity of achieving measurement z
+    '''
     if m == "⬛":
         if z == "⬜":
-            return 0.1
+            return 0.05
         elif z == "⬛":
-            return 0.9
+            return 0.95
 
     elif m == "⬜":
         if z == "⬜":
-            return 0.7
+            return 0.9
         elif z == "⬛":
-            return 0.3
+            return 0.1
     
-def scheduleModel(S : list[g1d], x : int, t : float , m : str = "⬛"):
-    #returns proability of m being occupied given schedule and time
+def scheduleModel(S : list[g1d], x : int, t : float , m : str = "⬛") -> float:
+    '''
+    S - schedule of building
+    x - location of cell
+    t - world time
+    
+    returns probablity of m given schedule
+    '''
     s = S[x]
     if m == "⬛":
             return s.cdf(t)
@@ -37,3 +52,37 @@ def scheduleModel(S : list[g1d], x : int, t : float , m : str = "⬛"):
             return 1 - s.cdf(t)
     else:
         raise Exception("m is not ⬜ or ⬛")
+
+def sampleMeasurement(x : int, buildingState : list[bool]) -> str:
+    '''
+    x - location
+    buildingState - list of booleans. True if built
+    
+    returns measurement z
+    '''
+
+    p = np.random.rand()
+    
+    if buildingState[x] == "⬛": #occupied
+        if p < forwardSensorModel(z = "⬛", m = "⬛"):
+            return "⬛"
+        else:
+            return "⬜"
+
+    if buildingState[x] == "⬜": #free
+        if p < forwardSensorModel(z = "⬜", m = "⬜"):
+            return "⬜"
+        else:
+            return "⬛"
+
+def updateMapping(z : str, S : list[g1d], x : int, t : float):
+    if z:
+        return p2logodds(inverseSensorScheduleModel(z,S,x,t,m = "⬛")) -p2logodds(scheduleModel(S, x, t, m = "⬛"))
+    else:
+        return +p2logodds(scheduleModel(S, x, t, m = "⬛"))
+
+def p2logodds(p):
+    return np.log(p / (1 - p + EPS))
+
+def logodds2p(l):
+    return  np.exp(l) / (1 + np.exp(l))
--- tests/05_mapping.py
+++ tests/05_mapping.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from TBBF.gaussians import gaussian1D as g1d
+from TBBF.models import updateMapping, sampleMeasurement, logodds2p, p2logodds
 from TBBF.plotting import show_gtScheduleMeasurement
 n = 5
 
@@ -8,28 +9,45 @@ np.random.seed(2)
 
 schedule = [g1d(3,0.5),
             g1d(4,1),
-            g1d(1e10,1), #build far far in the future
+            g1d(50,1), #build far far in the future
             g1d(5,0.5),
             g1d(6,1),
-            g1d(1e10,1), #build far far in the future
+            g1d(50,1), #build far far in the future
             g1d(7,0.2)]
-actual = np.array([g.sample() for g in schedule])
+history = np.array([g.sample() for g in schedule])
 
-t = 4.0
+
+t = 7.0
 dt = 1.0
+building = ["⬛" if int(t > h) else "⬜" for h in history]
 actions = [1,1,0,-1,-1,0,-1]
-measurements = ["⬛","⬜","⬛","⬜","⬜","⬛","⬜"]
+logodds_estimation = p2logodds(np.array([g.cdf(t) for g in schedule]))
+print(logodds_estimation)
 x = 2
 
-for a,z in zip(actions,measurements):
-    #move robot
-    #sample schedule
+for a in actions:
     x += a
-    s = np.array([g.cdf(t) for g in schedule])
-    building = np.array([int(t > ac) for ac in actual])
+    
+    z = sampleMeasurement(x,building)
+
+    #update mapping
+    for i in range(len(logodds_estimation)):
+        if x == i:
+            logodds_estimation[i] += updateMapping(z, schedule, i, t)
+        else:
+            logodds_estimation[i] += updateMapping('', schedule, i, t)
+
+    print(logodds_estimation[6])
+    #update building state
+    building = ["⬛" if int(t > h) else "⬜" for h in history]
+    showbuilding = np.array([b == "⬛" for b in building])
+    
+    #plot
+    # s = np.array([g.cdf(t) for g in schedule])
+    
     plt.cla()
-    show_gtScheduleMeasurement(building,x,s,z,t)
+    show_gtScheduleMeasurement(showbuilding,x,logodds2p(logodds_estimation),z,t)
     plt.pause(1)
-    t += dt
+    # t += dt
 
 plt.show()
